b5f2d4501ead248b530cd6be85a98cbe3997107b
Mark Norton <remillard@gmail.com>
2019-03-07 16:35:48 -0600

    Build Version TCL Script

---
+++ Build Version Package Generator/run_update_build_time.tcl
@@ -0,0 +1,11 @@
+# Runs the Python update_build_time.py script to update the
+# system build package.
+proc call_python {} {
+	set output [exec python ../bin/update_build_time.py -o ../src/system_build_info_pkg.vhd]
+	puts $output
+}
+post_message "Executing run_update_build_time.tcl script..."
+post_message "=================================================================="
+post_message "Running system build update Python program"
+call_python
+post_message "=================================================================="
