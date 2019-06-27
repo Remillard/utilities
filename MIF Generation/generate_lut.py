13f2224f01a0996366b349b053986b1cca50f135
Mark Norton <remillard@gmail.com>
2019-06-27 13:28:08 -0500

    Added value scaling to look up table generation

--- MIF Generation/generate_lut.py
+++ MIF Generation/generate_lut.py
@@ -15,7 +15,10 @@ def generate_mif(args, filename):
     # Max value is constrained to maximum positive value for 2's complement
     # numbers.
     depth = 2 ** args.depth
-    max_value = 2 ** (args.width - 1) - 1
+    if args.scale is not None:
+        max_value = args.scale
+    else:
+        max_value = 2 ** (args.width - 1) - 1
     if args.rotation == "full":
         angle_step = 2 * math.pi / depth
     else:
@@ -58,7 +61,10 @@ def generate_mem(args, filename):
     # Max value is constrained to maximum positive value for 2's complement
     # numbers.
     depth = 2 ** args.depth
-    max_value = 2 ** (args.width - 1) - 1
+    if args.scale is not None:
+        max_value = args.scale
+    else:
+        max_value = 2 ** (args.width - 1) - 1
     if args.rotation == "full":
         angle_step = 2 * math.pi / depth
     else:
@@ -90,7 +96,7 @@ def main():
     """Main entry point for program."""
     parser = argparse.ArgumentParser(
         prog="generate_lut",
-        description="""Creates a memory initialization file (*.mif) for a sinusoid.""",
+        description="""Creates a memory initialization file for a sinusoid.""",
     )
     parser.add_argument(
         "-p",
@@ -108,7 +114,7 @@ def main():
     parser.add_argument(
         "-w",
         "--width",
-        help="Data witdth specified in number of data bits.  Default: 16",
+        help="Data width specified in number of data bits.  Default: 16",
         default=16,
         type=int,
     )
@@ -118,6 +124,12 @@ def main():
         choices=["sin", "cos"],
         help="Function to be created.  Valid options are 'sin' and 'cos'.  Default: sin",
         default="sin",
+    )
+    parser.add_argument(
+        "-s",
+        "--scale",
+        help="Full scale value.  Must be less than 2^(width-1)-1.  Default: 2^(width-1)-1",
+        type=int
     )
     parser.add_argument(
         "-r",
@@ -136,17 +148,27 @@ def main():
     # TODO: Add other RADIX arguments someday instead of just HEX.
     args = parser.parse_args()
 
-    # Construct filename
-    depth = 2 ** args.depth
-    filename = "{}_{}x{}_{}_{}_lut.{}".format(
-        args.prefix, args.width, depth, args.rotation, args.function, args.format
-    )
-    print("Generating {}".format(filename))
-
-    if args.format == "mif":
-        generate_mif(args, filename)
+    # Error check for full scale value.  args.scale equivalent to None is
+    # fine, however if specified, must not be greater than the maximum
+    # possible value.
+    if args.scale is not None and args.scale > 2 ** (args.width - 1) - 1:
+        print("Argument Error: Full scale value must be less than or equal to the maximum possible.  A signed number at {} bits has a maximum scale value of {}.".format(args.width, 2**(args.width-1)-1))
     else:
-        generate_mem(args, filename)
+        # Construct filename
+        if args.scale is None:
+            scale_str = ""
+        else:
+            scale_str = "scaled"
+        depth = 2 ** args.depth
+        filename = "{}_{}x{}_{}_{}_{}_lut.{}".format(
+            args.prefix, args.width, depth, scale_str, args.rotation, args.function, args.format
+        )
+        print("Generating {}".format(filename))
+
+        if args.format == "mif":
+            generate_mif(args, filename)
+        else:
+            generate_mem(args, filename)
 
 
 if __name__ == "__main__":
