37567a3bf2de3ce20989963ef28da000775f8645
Mark Norton <remillard@gmail.com>
2021-01-14 12:56:14 -0600

    Some tweaks to the lookup table generation program

--- MIF Generation/generate_lut.py
+++ MIF Generation/generate_lut.py
@@ -10,22 +10,33 @@ import argparse
 import math
 
 
-def generate_mif(args, filename):
+def generate_file(args, filename):
     """Creating a MIF formatted memory file based on the arguments."""
     # Max value is constrained to maximum positive value for 2's complement
     # numbers.
-    depth = 2 ** args.depth
     if args.scale is not None:
         max_value = args.scale
     else:
-        max_value = 2 ** (args.width - 1) - 1
-    if args.rotation == "full":
-        angle_step = 2 * math.pi / depth
-    else:
-        angle_step = 0.5 * math.pi / depth
+        max_value = 2 ** (args.datawidth - 1) - 1
 
-    addr_nibbles = math.ceil(args.depth / 4)
-    data_nibbles = math.ceil(args.width / 4)
+    # TODO Actually support the num samples option!
+    # Adding the endpoint means we need to squeeze both the origin and end in,
+    # which means we need to shorten the angle step by one address.
+    depth = 2 ** args.addrwidth
+    if args.endpoint:
+        num_samples = depth - 1
+    else:
+        num_sample = depth
+
+    if args.rotation == "full":
+        angle_step = 2 * math.pi / num_samples
+    elif args.rotation == "quad":
+        angle_step = 0.5 * math.pi / num_samples
+    elif args.rotation == "eighth":
+        angle_step = 0.25 * math.pi / num_samples
+
+    addr_nibbles = math.ceil(args.addrwidth / 4)
+    data_nibbles = math.ceil(args.datawidth / 4)
 
     if args.function == "sin":
         func = math.sin
@@ -34,66 +45,59 @@ def generate_mif(args, filename):
 
     lut_file = open(filename, "w")
 
-    lut_file.write("DEPTH={}; % Memory Depth in Address Locations %\n".format(depth))
-    lut_file.write("WIDTH={}; % Memory Width in Bits %\n".format(args.width))
+    if args.format == "mif":
+        write_mif_header(lut_file, depth, args.datawidth)
+    elif args.format == "mem":
+        write_mem_header(lut_file, depth, args.datawidth)
+
+    # TODO: Support multiple values per line.
+    for idx in range(0, depth):
+        angle = idx * angle_step
+        value = round(max_value * func(angle))
+        # Converte negative numbers to 16 bit 2's complement
+        if value < 0:
+            value = value + 2 ** args.datawidth
+
+        if args.format == "mif":
+            write_mif_line(lut_file, idx, addr_nibbles, value, data_nibbles)
+        elif args.format == "mem":
+            write_mem_line(lut_file, value, data_nibbles)
+
+    if args.format == "mif":
+        write_mif_footer(lut_file)
+
+    lut_file.close()
+
+
+def write_mif_header(lut_file, depth, datawidth):
+    """Specific header to MIF file formats."""
+    lut_file.write(f"DEPTH={depth}; % Memory Depth in Address Locations %\n")
+    lut_file.write(f"WIDTH={datawidth}; % Memory Width in Bits %\n")
     lut_file.write("ADDRESS_RADIX = HEX;\n")
     lut_file.write("DATA_RADIX = HEX;\n")
     lut_file.write("CONTENT\nBEGIN\n")
 
-    # TODO: Support multiple values per line.
-    for idx in range(0, depth):
-        line = "{:0{size}X} : ".format(idx, size=addr_nibbles)
-        angle = idx * angle_step
-        value = round(max_value * func(angle))
-        # Converte negative numbers to 16 bit 2's complement
-        if value < 0:
-            value = value + 2 ** args.width
-        line = line + "{:0{size}X} ".format(value, size=data_nibbles)
-        line = line + ";\n"
-        lut_file.write(line)
 
+def write_mif_line(lut_file, address, addr_nibbles, data, data_nibbles):
+    line = f"{address:0{addr_nibbles}X} : {data:0{data_nibbles}X} ;\n"
+    lut_file.write(line)
+
+
+def write_mif_footer(lut_file):
     lut_file.write("END;\n")
-    lut_file.close()
 
 
-def generate_mem(args, filename):
-    """Given a filename and parameters, generates the MEM formatted look up table."""
-    # Max value is constrained to maximum positive value for 2's complement
-    # numbers.
-    depth = 2 ** args.depth
-    if args.scale is not None:
-        max_value = args.scale
-    else:
-        max_value = 2 ** (args.width - 1) - 1
-    if args.rotation == "full":
-        angle_step = 2 * math.pi / depth
-    else:
-        angle_step = 0.5 * math.pi / depth
-
-    data_nibbles = math.ceil(args.width / 4)
-
-    if args.function == "sin":
-        func = math.sin
-    else:
-        func = math.cos
-
-    lut_file = open(filename, "w")
+def write_mem_header(lut_file, depth, datawidth):
+    """Specific header to Verilog MEM file format."""
     lut_file.write("// Verilog Hex Memory Format\n")
-    lut_file.write("// DEPTH={}\n".format(depth))
-    lut_file.write("// WIDTH={}\n".format(args.width))
+    lut_file.write(f"// DEPTH={depth}\n")
+    lut_file.write(f"// WIDTH={datawidth}\n")
     lut_file.write("// DATA_RADIX = HEX\n")
 
-    for idx in range(0, depth):
-        line = ""
-        angle = idx * angle_step
-        value = round(max_value * func(angle))
-        # Converte negative numbers to 16 bit 2's complement
-        if value < 0:
-            value = value + 2 ** args.width
-        line = line + "{:0{size}X}\n".format(value, size=data_nibbles)
-        lut_file.write(line)
 
-    lut_file.close()
+def write_mem_line(lut_file, data, data_nibbles):
+    line = f"{data:0{data_nibbles}X}\n"
+    lut_file.write(line)
 
 
 def main():
@@ -108,16 +112,29 @@ def main():
         help="MIF output filename prefix.  Default: 'rom'",
         default="rom",
     )
-    parser.add_argument(
-        "-d",
-        "--depth",
-        help="Memory depth specified in number of address bits.  Default: 12",
+    mem_depth_group = parser.add_mutually_exclusive_group()
+    mem_depth_group.add_argument(
+        "-aw",
+        "--addrwidth",
+        help="""Memory depth specified in the width of the address.  The number
+        of samples will be 2 ^ addrwidth.  Mutually exclusive option with number
+        of samples.  Default: 12""",
+        default=12,
+        type=int,
+    )
+    mem_depth_group.add_argument(
+        "-ns",
+        "--numsamples",
+        help="""Memory depth specified in number of look up table samples.  The
+        number of address bits will be calculated to be 2 ^ ceiling(log2(numsamples)).
+        Address values not covered will be set to 0.  Mutually exclusive option
+        with depth.  Default: 4096""",
         default=12,
         type=int,
     )
     parser.add_argument(
-        "-w",
-        "--width",
+        "-dw",
+        "--datawidth",
         help="Data width specified in number of data bits.  Default: 16",
         default=16,
         type=int,
@@ -126,28 +143,47 @@ def main():
         "-f",
         "--function",
         choices=["sin", "cos"],
-        help="Function to be created.  Valid options are 'sin' and 'cos'.  Default: sin",
+        help="""Function to be created.  Valid options are 'sin' and 'cos'.
+        Default: sin""",
         default="sin",
     )
     parser.add_argument(
         "-s",
         "--scale",
-        help="Full scale value.  Must be less than 2^(width-1)-1.  Default: 2^(width-1)-1",
+        help="""Full scale value.  Must be less than 2^(datawidth-1)-1.
+        Default: 2^(datawidth-1)-1""",
         type=int,
     )
     parser.add_argument(
         "-r",
         "--rotation",
-        choices=["full", "quad"],
-        help="Rotational angle.  Valid options are 'full' and 'quad'.  Default: full",
+        choices=["full", "quad", "eighth"],
+        help="""Rotational angle.  Valid options are 'full' and 'quad' and
+        'eighth'.  Note that the eighth mode does not automatically generate
+        both sin/cos outputs but that's how it's intended to be done, so run
+        the program again to generate the other function.  Default: full""",
         default="full",
     )
     parser.add_argument(
         "-fmt",
         "--format",
         choices=["mif", "mem"],
-        help="Sets the output file format.  MIF is the Intel Memory Initialization File format (similar to MTI as well).  MEM is the Verilog Hex Memory format.  Default: mif",
+        help="""Sets the output file format.  MIF is the Intel Memory
+        Initialization File format (similar to MTI as well).  MEM is the
+        Verilog Hex Memory format.  Default: mif""",
         default="mif",
+    )
+    parser.add_argument(
+        "-ep",
+        "--endpoint",
+        action="store_true",
+        help="""This option adjusts the address to angle ratio slightly by
+        altering the angle step per address to include the endpoint.  Normally
+        the sweep will run [sin(0), sin(2*pi)).  This is primarily useful for
+        the quarter and eighth options.  For example with an eighth rotation
+        sine sweep and 12 address bits, 16 data bits, the value at 0xfff will
+        be (2^15 - 1) * sin(pi/4) = 0x5a82.  Care may need to be taken with the
+        angle determination.  Default: Do not include the endpoint."""
     )
     # TODO: Add other RADIX arguments someday instead of just HEX.
     args = parser.parse_args()
@@ -167,10 +203,16 @@ def main():
             scale_str = ""
         else:
             scale_str = "scaled_"
-        depth = 2 ** args.depth
+
+        # TODO: Actually support the number of samples option
+        if args.addrwidth:
+            depth = 2 ** args.addrwidth
+        else:
+            print("Error: Nonbinary depth not yet supported despite the fact it's in the help text.")
+
         filename = "{}_{}x{}_{}{}_{}_lut.{}".format(
             args.prefix,
-            args.width,
+            args.datawidth,
             depth,
             scale_str,
             args.rotation,
@@ -179,10 +221,7 @@ def main():
         )
         print("Generating {}".format(filename))
 
-        if args.format == "mif":
-            generate_mif(args, filename)
-        else:
-            generate_mem(args, filename)
+        generate_file(args, filename)
 
 
 if __name__ == "__main__":
